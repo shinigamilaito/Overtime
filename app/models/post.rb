0660495f61638a3edeba157c0ad1ddcdbc1b415c
Edgar Eduardo Espindola <edgareduardoespindola@gmail.com>
2019-12-18 07:05:01 -0600

    Implementes user connection to posts with test

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ApplicationRecord
+  belongs_to :user
+
   validates :date, :rationale, presence: true
 end
---
+++ db/migrate/20191213115835_add_users_to_posts.rb
@@ -0,0 +1,5 @@
+class AddUsersToPosts < ActiveRecord::Migration[5.1]
+  def change
+    add_reference :posts, :user, foreign_key: true, index: true
+  end
+end
