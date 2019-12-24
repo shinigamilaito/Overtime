4c3b6e87a66e3948791a8938a034859b9473983b
Edgar Eduardo Espindola <edgareduardoespindola@gmail.com>
2019-12-24 14:56:54 -0600

    Implemented edit and update functionality

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'navigate' do
   before do
-    @user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "John", last_name: 'Snow')
+    @user = FactoryGirl.create(:user)
     scope = Devise::Mapping.find_scope!(@user)
     login_as(@user, scope: scope)
   end
@@ -21,10 +21,10 @@ describe 'navigate' do
     end
 
     it 'has a list of posts' do
-      post1 = Post.create(date: Date.today, rationale: "Post1", user: @user)
-      post2 = Post.create(date: Date.today, rationale: "Post2", user: @user)
+      FactoryGirl.build_stubbed(:post)
+      FactoryGirl.build_stubbed(:second_post)
       visit posts_path
-      expect(page).to have_content(/Post1|Post1/)
+      expect(page).to have_content(/Rationale|content/)
     end
   end
 
@@ -53,6 +53,29 @@ describe 'navigate' do
       click_on "Save"
 
       expect(User.last.posts.last.rationale).to eq("User Association")
+    end
+  end
+
+  describe 'edit' do
+    before do
+      @post = FactoryGirl.create(:post)
+    end
+
+    it 'can be reached by clicking edit on index page' do
+      visit posts_path
+
+      click_link("edit_post_#{@post.id}")
+      expect(page.status_code).to eq(200)
+    end
+
+    it "can be edited" do
+      visit edit_post_path(@post)
+
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: 'Edited content'
+      click_on 'Save'
+
+      expect(page).to have_content("Edited content")
     end
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -31,6 +31,7 @@ RSpec.configure do |config|
   config.after(:each) { DatabaseCleaner.clean }
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
+  config.include FactoryGirl::Syntax::Methods
 
   config.expect_with :rspec do |c|
     c.syntax = :expect
