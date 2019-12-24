bca8ab1034ce9bf0c287bbf1cf3b7f50fbc0c22a
Edgar Eduardo Espindola <edgareduardoespindola@gmail.com>
2019-12-24 10:46:16 -0600

    Implemented custom full name method to the user

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :first_name, :last_name, presence: true
+
+  def full_name
+    last_name.upcase + ", " + first_name.upcase
+  end
 end
