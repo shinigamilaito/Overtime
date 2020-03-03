f7af39e72ac0553c3d35dbe9a33a06be8b79d1ec
Edgar Eduardo Espindola <edgareduardoespindola@gmail.com>
2020-03-03 14:38:17 -0600

    Integrated secrets enc

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,4 +53,9 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  # Attempt to read encrypted secrets from `config/secrets.yml.enc`.
+  # Requires an encryption key in `ENV["RAILS_MASTER_KEY"]` or
+  # `config/secrets.yml.key`.
+  config.read_encrypted_secrets = true
 end
