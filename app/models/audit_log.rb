85a05eac9835db98137d09a780d0ead958817cc6
Edgar Eduardo Espindola <edgareduardoespindola@gmail.com>
2020-03-04 16:31:41 -0600

    Integrated validations and defaults for audit log attributes

--- app/models/audit_log.rb
+++ app/models/audit_log.rb
@@ -1,3 +1,13 @@
 class AuditLog < ApplicationRecord
   belongs_to :user
+
+  validates :status, :start_date, presence: true
+
+  after_initialize :set_defaults
+
+  private
+
+  def set_defaults
+    self.start_date ||=  Date.today - 6.days
+  end
 end
