7d2b04a34a894c03d8c7acfe556a19b5585b458f
Edgar Eduardo Espindola <edgareduardoespindola@gmail.com>
2020-03-10 09:49:43 -0600

    Implemented pagination for posts and audit log

--- app/controllers/audit_logs_controller.rb
+++ app/controllers/audit_logs_controller.rb
@@ -1,6 +1,6 @@
 class AuditLogsController < ApplicationController
   def index
     authorize AuditLog
-    @audit_logs = AuditLog.all
+    @audit_logs = AuditLog.page(params[:page]).per(10)
   end
 end
