20701169d06ca573db7276960ee09abbf071c779
Edgar Eduardo Espindola <edgareduardoespindola@gmail.com>
2020-03-17 12:13:14 -0600

    Refactored button functionality for employee dashboard

--- app/models/audit_log.rb
+++ app/models/audit_log.rb
@@ -8,6 +8,8 @@ class AuditLog < ApplicationRecord
   after_initialize :set_defaults
   before_update :set_end_date, if: :confirmed?
 
+  scope :by_start_date, -> { order(created_at: :desc) }
+
   private
 
   def set_end_date
